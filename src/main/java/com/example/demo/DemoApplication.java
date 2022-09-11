99916024b3362499a22a3db573f2ff96180cd1f9
user <pravatpradhan72@gmail.com>
2022-09-12 02:08:05 +0800

    remove code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,19 +15,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		List<String> sheets = new ArrayList<>(2);
-		sheets.add("b");
-		sheets.add("c");
-
-
-
-		try {
-			File excel = ResourceUtils.getFile("classpath:Book1.xlsx").getAbsoluteFile();
-			DocConvertorXLSXToCSV.conversionOfXlsxToCSVNames(excel.getAbsolutePath(), "test.csv", sheets);
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-			e.printStackTrace();
-		}
 	}
 
 }
